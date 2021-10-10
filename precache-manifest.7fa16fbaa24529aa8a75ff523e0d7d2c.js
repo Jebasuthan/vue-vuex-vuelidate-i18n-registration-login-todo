c3500b01fafee758fde8af762bf06db64490c009
Jeba Suthan <itjebasuthan@gmail.com>
2021-10-10 16:55:25 +0530

    deploy

---
+++ precache-manifest.7fa16fbaa24529aa8a75ff523e0d7d2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f018c73cde0b8e7988",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/css/app.c4dae8c3.css"
+  },
+  {
+    "revision": "0494701fc483dbecb1aa",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/css/chunk-vendors.0ba3e87d.css"
+  },
+  {
+    "revision": "2b156a14b9ddd032e75de199aba4591c",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/img/user-dummy.2b156a14.png"
+  },
+  {
+    "revision": "fa0cbd3d5bfc1c4035dbb98b74633d9d",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/index.html"
+  },
+  {
+    "revision": "e8f018c73cde0b8e7988",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/js/app.be89bfba.js"
+  },
+  {
+    "revision": "0494701fc483dbecb1aa",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/js/chunk-vendors.c8c3b298.js"
+  },
+  {
+    "revision": "64aecfb6f7e4566033872e329a54e822",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-vuex-vuelidate-i18n-registration-login-todo/robots.txt"
+  }
+]);
